724c6f9c970910d697ec749ca44dee9458847ebd
Rashid <rashidnzri@gmail.com>
2025-12-09 13:28:40 +0330

    Create http.ts

---
+++ http.ts
@@ -0,0 +1,10 @@
+/**
+ * Helper for consistent API responses
+ */
+export function success(data: any) {
+  return { success: true, data };
+}
+
+export function failure(message: string) {
+  return { success: false, error: message };
+}
