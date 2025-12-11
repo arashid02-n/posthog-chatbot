9b7c6a5e486325d3e5dd6e68a69e0066632d28ba
Rashid <rashidnzri@gmail.com>
2025-12-11 14:22:18 +0330

    Update client.ts

--- server/src/posthog/client.ts
+++ server/src/posthog/client.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const posthogClient = axios.create({
-  baseURL: process.env.POSTHOG_API_URL,
+  baseURL: process.env.POSTHOG_API_URL, 
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.POSTHOG_API_KEY}`,
