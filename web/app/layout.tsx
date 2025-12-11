c1ea8115a3e93bee8fca0074f81b2312d0e31279
Rashid <rashidnzri@gmail.com>
2025-12-11 16:12:25 +0330

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,7 +1,5 @@
-// web/app/layout.tsx
-
-import './globals.css'
-import Script from "next/script"
+import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "PostHog Chatbot",
@@ -11,18 +9,53 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
+      <body>
+
+        {/* PostHog Snippet */}
         <Script id="posthog-init" strategy="afterInteractive">
           {`
-            !function(t,e){var o,n,p,r;e.__SV||(window.posthog && window.posthog.__loaded)||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init zr Wr fi Br Gr ci Nr Hr capture Ui calculateEventProperties Kr register register_once register_for_session unregister unregister_for_session Zr getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey displaySurvey cancelPendingSurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupProperties reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty Xr Jr createPersonProfile Qr jr ts opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing get_explicit_consent_status is_capturing clear_opt_in_out_capturing Vr debug O Yr getPageViewId captureTraceFeedback captureTraceMetric Or".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+            !function(t,e){var o,n,p,r;
+            if(!e.__SV){
+              window.posthog = e; e._i = [];
+              e.init=function(i,s,a){
+                function g(t,e){
+                  var o=e.split(".");
+                  if(o.length==2){ t=t[o[0]]; e=o[1]; }
+                  t[e]=function(){
+                    t.push([e].concat(Array.prototype.slice.call(arguments,0)));
+                  }
+                }
+                p=t.createElement("script");
+                p.type="text/javascript"; p.async=!0; p.crossOrigin="anonymous";
+                p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js";
+                r=t.getElementsByTagName("script")[0];
+                r.parentNode.insertBefore(p,r);
+                
+                var u=e;
+                if(a){ u=e[a]=[]; } else { a="posthog"; }
+                u.people=u.people||[];
+                
+                var funcs="init capture identify alias ...".split(" ");
+                for(o=0;o<funcs.length;o++) g(u,funcs[o]);
+
+                e._i.push([i,s,a]);
+              };
+              e.__SV=1;
+            }
+          `}
+        </Script>
+
+        <Script id="posthog-config" strategy="afterInteractive">
+          {`
             posthog.init('phc_9LhkwALp1yKFMroS5JzNMXe7OvkmO4LdepFj5m6zX1X', {
               api_host: 'https://us.i.posthog.com',
-              person_profiles: 'identified_only'
+              autocapture: true
             });
           `}
         </Script>
-      </head>
-      <body>{children}</body>
+
+        {children}
+      </body>
     </html>
   );
 }
