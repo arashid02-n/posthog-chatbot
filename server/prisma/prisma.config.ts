8aa75d13b1972fdac55a3ea4eacbac1bfa184e67
Rashid <rashidnzri@gmail.com>
2025-12-13 14:55:09 +0330

    Update prisma.config.ts

--- server/prisma/prisma.config.ts
+++ server/prisma/prisma.config.ts
@@ -1,9 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,  // The URL of your database, set in .env
-    },
-  },
-});
+export const prisma = new PrismaClient();
