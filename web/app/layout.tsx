61b93481e2bb625b156bddb1968736374ce46c01
Rashid <rashidnzri@gmail.com>
2025-12-15 16:29:26 +0330

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,24 +1,30 @@
-"use client";
+import "./globals.css";
+import Script from "next/script";
 
-import posthog from "posthog-js";
-import { useEffect } from "react";
+export const metadata = {
+  title: "PostHog Chatbot",
+  description: "Analytics + AI",
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      posthog.init("phc_9LhkwALp1yKFMroS5JzNMXe7OvkmO4LdepFj5m6zX1X", {
-        api_host: "https://us.i.posthog.com",
-        capture_pageview: true,
-      });
-    }
-  }, []);
-
   return (
     <html lang="en">
+      <head>
+        {/* PostHog init (SAFE + OFFICIAL WAY) */}
+        <Script id="posthog-init" strategy="beforeInteractive">
+          {`
+            !function(t,e){var o,n,p,r;e.__SV||(window.posthog && window.posthog.__loaded)||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture identify register register_once reset on off".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+            posthog.init("phc_9LhkwALp1yKFMroS5JzNMXe7OvkmO4LdepFj5m6zX1X", {
+              api_host: "https://us.i.posthog.com",
+              capture_pageview: true
+            });
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
