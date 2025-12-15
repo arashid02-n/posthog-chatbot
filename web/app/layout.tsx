5c09d9b95dd54a0b132db4486e85759df6ec2fd8
Rashid <rashidnzri@gmail.com>
2025-12-15 16:13:14 +0330

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -3,15 +3,22 @@
 import posthog from "posthog-js";
 import { useEffect } from "react";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   useEffect(() => {
-    posthog.init("phc_9LhkwALp1yKFMroS5JzNMXe7OvkmO4LdepFj5m6zX1X", {
-      api_host: "https://us.i.posthog.com",
-    });
+    if (typeof window !== "undefined") {
+      posthog.init("phc_9LhkwALp1yKFMroS5JzNMXe7OvkmO4LdepFj5m6zX1X", {
+        api_host: "https://us.i.posthog.com",
+        capture_pageview: true,
+      });
+    }
   }, []);
 
   return (
-    <html>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
